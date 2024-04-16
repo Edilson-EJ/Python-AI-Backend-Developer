9853bbd9b9f627f9eac116050fe49b4168f85cdb
Edilson-EJ <edilson.franca.melo@gmail.com>
2024-04-16 11:42:25 -0300

    entrada-saida

---
+++ 2-conhecendo-linguagem/funcoes-entrada-saida.py
@@ -0,0 +1,12 @@
+nome = input("Informe o seu nome:")
+
+print("seu nome e:", nome)
+
+nome_1 = "edilson"
+sobrenome = "melo"
+
+print(nome_1,sobrenome)
+
+print(nome_1,sobrenome, end="...\n")
+
+print(nome_1,sobrenome, sep="#")
