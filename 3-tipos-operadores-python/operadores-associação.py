793ae128bfdbd9130e325518490be4fecd6432ce
Edilson-EJ <edilson.franca.melo@gmail.com>
2024-04-23 18:58:30 -0300

    apredendo sobre operadores

---
+++ 3-tipos-operadores-python/operadores-associação.py
@@ -0,0 +1,7 @@
+curso = "curso de python"
+frutas = ["laranja", "uva", "limão"]
+saques = [1500,100]
+
+print("python" in curso)
+
+print("MAÇA" not in frutas)
---
+++ 3-tipos-operadores-python/operadores-logicos.py
@@ -0,0 +1,21 @@
+# exemplo
+
+saldo = 1000
+saque = 200
+limite = 100
+
+print("saldo e maior que saque", saldo >= saque)
+
+print("saque e menor que limite", saque <= limite)
+
+# opreador E
+
+print("usando o AND", saldo >= saque and saque <= limite)
+
+# operador ou
+
+print("usando o OR", saldo >= saque or saque <= limite)
+
+# operador negação
+
+print("1000 e maior 1500", not 1000 > 1500)
